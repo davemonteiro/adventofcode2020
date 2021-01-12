497c585be3c9ace05549089bb36b079008ec9eee
Dave Monteiro <davemonteiro17@gmail.com>
2021-01-12 11:00:58 -0500

    added problem statements from aoc site

--- 01.py
+++ 01.py
@@ -1,22 +1,13 @@
-###
-# --- Day 1: Report Repair ---
-#
-# Part 1: Given a list of integers, return the product of the two of them that sum to 2020.
-#
-# Part 2: Given a list of integers, return the product of the three of them that sum to 2020.
-#
-###
-
 def sum2(arr: list, total: int) -> int:
-    #Returns the product of the two distinct integers in a list that sum to 'total'
+    #Returns the product of the two distinct integers in arr that sum to total
     for i in arr:
         if (total - i) in arr:
-            return i*(total-i)
+            return i * (total - i)
 
 def sum3(arr: list, total: int) -> int:
-    #Returns the product of the three distinct integers in a list that sum to 'total'
+    #Returns the product of the three distinct integers in arr that sum to total
     for i in range(0, len(arr)):
-        for j in range(i+1, len(arr)):
+        for j in range(i + 1, len(arr)):
             missing_int = (total - arr[i] - arr[j])
             if missing_int in arr and arr[i] != missing_int and arr[j] != missing_int:
                 return (arr[i] * arr[j] * missing_int)
@@ -38,3 +29,32 @@ print('Part 2: ', sum3(expenses, 2020))
 end = time.time()
 print('Time elapsed: ', end-start)
 #<0.01
+
+"""
+--- Day 1: Report Repair ---
+
+Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); 
+apparently, something isn't quite adding up. Specifically, they need you to find the two entries that sum to 
+2020 and then multiply those two numbers together. For example, suppose your expense report contained the following:
+
+1721
+979
+366
+299
+675
+1456
+
+In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 
+1721 * 299 = 514579, so the correct answer is 514579. Of course, your expense report is much larger. 
+
+Find the two entries that sum to 2020; what do you get if you multiply them together?
+
+--- Part Two ---
+
+The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left 
+over from a past vacation. They offer you a second one if you can find three numbers in your expense report
+that meet the same criteria. Using the above example again, the three entries that sum to 2020 are 979, 366,
+and 675. Multiplying them together produces the answer, 241861950.
+
+In your expense report, what is the product of the three entries that sum to 2020?
+"""
